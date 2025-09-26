4061c13cb0e7df292dd9b2cb50500a1890b2a2d9
jonathan fitzgerald <jfitzg09@calpoly.edu>
2025-09-25 23:39:45 -0700

    finished

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
 import Table from "./Table";
-
+import Form from "./Form";
   
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-            name: "Charlie",
-            job: "Janitor"
-        }
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -21,6 +16,7 @@ function MyApp() {
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form />
       </div>
     );
   }
