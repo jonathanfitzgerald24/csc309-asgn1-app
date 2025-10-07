796f1ddcb95226ce6a18e4af371d9d9b8954ee62
jonathan fitzgerald <jfitzg09@calpoly.edu>
2025-10-07 16:25:35 -0700

    ready to link

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,7 +39,27 @@ const findUserByName = (name) => {
       (user) => user["name"] === name
     );
   };
-  
+
+const findUsersByNameAndJob = (name, job) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name && user["job"] === job
+    );
+  };
+
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);  
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+
+const deleteUser = (id) => {
+    users["users_list"] = users["users_list"].filter((user) => user["id"] !== id);
+    return users["users_list"];
+  };
+
+
 app.get("/users", (req, res) => {
 const name = req.query.name;
 if (name != undefined) {
@@ -49,6 +69,47 @@ if (name != undefined) {
 } else {
     res.send(users);
 }
+});
+
+  
+app.get("/users/:id", (req, res) => {
+const id = req.params["id"]; //or req.params.id
+let result = findUserById(id);
+if (result === undefined) {
+    res.status(404).send("Resource not found.");
+} else {
+    res.send(result);
+}
+});
+
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
+
+app.delete("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    deleteUser(id);
+    res.send();
+  });
+
+app.get("/users", (req, res) => {
+    const name = req.query.name;
+    const job = req.query.job;
+    
+    if (name != undefined && job != undefined) {
+        let result = findUsersByNameAndJob(name, job);
+        result = { users_list: result };
+        res.send(result);
+    } else if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    } else {
+        res.send(users);
+    }
 });
 
 app.listen(port, () => {
